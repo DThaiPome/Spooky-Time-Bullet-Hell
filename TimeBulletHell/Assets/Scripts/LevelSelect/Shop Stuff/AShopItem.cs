efa9e3da80fbb2d2b4d4eff90d4dee889cd897b3
DThaiPome <r.gehrlein154@gmail.com>
2020-05-16 17:02:31 -0400

    Added an abstract scriptable object for shop items. This will get weird.

---
+++ TimeBulletHell/Assets/Scripts/LevelSelect/Shop Stuff/AShopItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AShopItem : ScriptableObject
+{
+    [SerializeField]
+    protected Sprite icon;
+    [SerializeField]
+    protected string itemName;
+    [SerializeField]
+    protected int price;
+
+    //Do something when purchased
+    public abstract void onPurchase();
+
+    public virtual Sprite getIcon()
+    {
+        return this.icon;
+    }
+
+    public virtual string getName()
+    {
+        return this.itemName;
+    }
+
+    public virtual int getPrice()
+    {
+        return this.price;
+    }
+}
---
+++ TimeBulletHell/Assets/Scripts/LevelSelect/Shop Stuff/InventoryShopItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryShopItem", menuName = "ShopItems/InventoryShopItem", order = 1)]
+public class InventoryShopItem : AShopItem
+{
+    [SerializeField]
+    protected InventoryShopItems item;
+
+    public override void onPurchase()
+    {
+        EventManager.instance.addToInventory(this.enumToItem());
+    }
+
+    protected virtual InventoryItem enumToItem()
+    {
+        switch(this.item)
+        {
+            case InventoryShopItems.TimelessGun:
+                return new TimelessGun();
+            default:
+                return null;
+        }
+    }
+}
+
+public enum InventoryShopItems
+{
+    TimelessGun
+}
