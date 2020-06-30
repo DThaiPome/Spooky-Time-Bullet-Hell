ecdac5e3fc0a14865237fde7af34d151b0b42fbc
DThaiPome <r.gehrlein154@gmail.com>
2020-06-30 18:04:51 -0400

    Made a mock of the prototype UI for the level select

---
+++ TimeBulletHell/Assets/Scripts/LevelSelect/LevelSelectStaticWindowContent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectStaticWindowContent : ALevelSelectWindowContents
+{
+    public override void hideContents()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public override void showContents()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
