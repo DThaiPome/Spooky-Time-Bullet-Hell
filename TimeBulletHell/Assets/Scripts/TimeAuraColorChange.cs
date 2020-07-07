766275eb64a7d888c7c91fc60c99bc5303e62cf7
DThaiPome <r.gehrlein154@gmail.com>
2020-07-07 16:57:04 -0400

    Added an aura that changes color based on the flow of time

---
+++ TimeBulletHell/Assets/Scripts/TimeAuraColorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeAuraColorChange : MonoBehaviour
+{
+    [SerializeField]
+    private Color normalColor;
+    [SerializeField]
+    private Color slowColor;
+
+    private PlayerMovement pm;
+    private Image image;
+
+    void Start()
+    {
+        this.pm = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        this.image = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.image.color = Color.Lerp(this.slowColor, this.normalColor, this.pm.getSpeedPercent());
+        this.image.color = new Color(this.image.color.r, this.image.color.g, this.image.color.b, 1);
+    }
+}
