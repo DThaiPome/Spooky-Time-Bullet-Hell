daffbe002d356c4a51639465438f8c324804985f
DThaiPome <r.gehrlein154@gmail.com>
2020-07-10 14:29:52 -0400

    Set up text getters for UI

---
+++ TimeBulletHell/Assets/Scripts/UI/GameUI/UpdateUIText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUIText : MonoBehaviour
+{
+    [Header("Lives")]
+    [SerializeField]
+    private AGetTextForUI livesGetter;
+    [SerializeField]
+    private Text livesText;
+    [Header("Coins")]
+    [SerializeField]
+    private AGetTextForUI coinsGetter;
+    [SerializeField]
+    private Text coinsText;
+    [Header("Score")]
+    [SerializeField]
+    private AGetTextForUI scoreGetter;
+    [SerializeField]
+    private Text scoreText;
+
+    void Update()
+    {
+        this.livesText.text = livesGetter.getText();
+        this.coinsText.text = coinsGetter.getText();
+        this.scoreText.text = scoreGetter.getText();
+    }
+}
