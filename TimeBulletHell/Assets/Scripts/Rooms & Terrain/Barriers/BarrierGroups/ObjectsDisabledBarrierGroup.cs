645796f07fba40f196132c9c66ea77505c23f5fc
DThaiPome <r.gehrlein154@gmail.com>
2020-07-21 16:07:53 -0400

    Set up everything necessary for the gate at the end of the level

---
+++ TimeBulletHell/Assets/Scripts/Rooms & Terrain/Barriers/BarrierGroups/ObjectsDisabledBarrierGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsDisabledBarrierGroup : ABarrierGroup
+{
+    [SerializeField]
+    private List<GameObject> objects;
+
+    protected override void update()
+    {
+        base.update();
+        
+        foreach(GameObject g in this.objects)
+        {
+            if (g.activeSelf)
+            {
+                return;
+            }
+        }
+
+        this.removeBarriers();
+    }
+}
