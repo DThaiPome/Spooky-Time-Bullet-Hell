845607ebd4e76d29b428cea7919de702f2d69d47
DThaiPome <r.gehrlein154@gmail.com>
2020-09-07 20:53:03 -0400

    Started boss, but we can't use coroutines as they are. Oops.

---
+++ TimeBulletHell/Assets/Scripts/Mobs/MobTypes/Bosses/BossMobWithPhases.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BossMobWithPhases : BossMobBehaviour
+{
+    private Queue<string> toDo;
+    protected bool busy { get; private set; }
+    protected string lastAction { get; private set; }
+
+    protected override void onEnable()
+    {
+        base.onEnable();
+        this.ResetQueue();
+        this.lastAction = "";
+    }
+
+    protected void AddToDo(string func)
+    {
+        this.toDo.Enqueue(func);
+    }
+
+    protected void EndAction(float restingTime)
+    {
+        this.Rest();
+        StartCoroutine(RestBeforeNextAction(restingTime));
+    }
+
+    private IEnumerator RestBeforeNextAction(float restingTime)
+    {
+        yield return new WaitForSeconds(restingTime);
+        this.busy = false;
+    }
+
+    protected void ResetQueue()
+    {
+        this.toDo = new Queue<string>();
+        this.busy = true;
+        this.EndAction(0.1f);
+    }
+
+    protected override void update()
+    {
+        base.update();
+        if (!this.busy && this.toDo.Count > 0)
+        {
+            this.busy = true;
+            string action = this.toDo.Dequeue();
+            this.StartCoroutine(action);
+            this.lastAction = action;
+            Debug.Log(action);
+        }
+    }
+
+    protected abstract void Rest();
+
+    protected void AddRandomToDo(List<string> funcs, List<float> odds)
+    {
+        float r = Random.Range(0f, 1f);
+        odds = this.NormalizeOdds(odds);
+        for(int i = 0; i < funcs.Count && i < odds.Count; i++)
+        {
+            if (r <= odds[i])
+            {
+                this.AddToDo(funcs[i]);
+                return;
+            }
+        }
+    }
+
+    private List<float> NormalizeOdds(List<float> odds)
+    {
+        List<float> result = new List<float>(odds);
+        float sum = 0;
+        for(int i = 0; i < result.Count; i++)
+        {
+            float prevSum = sum;
+            sum += result[i];
+            result[i] += prevSum;
+        }
+        for(int i = 0; i < result.Count; i++)
+        {
+            result[i] /= sum;
+        }
+        return result;
+    }
+
+    protected bool IsToDoEmpty()
+    {
+        return this.toDo.Count == 0;
+    }
+}
--- TimeBulletHell/Assets/Scripts/Mobs/MobTypes/Bosses/StarNosedMole.cs
+++ TimeBulletHell/Assets/Scripts/Mobs/MobTypes/Bosses/StarNosedMole.cs
@@ -2,17 +2,115 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarNosedMole : MonoBehaviour
+public class StarNosedMole : BossMobWithPhases
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float coverFaceLength = 2f;
+
+    private StarNosedMoleState state;
+
+    private bool coveringFace;
+
+    protected override void onEnable()
     {
-        
+        base.onEnable();
+        this.state = StarNosedMoleState.ExposedI;
+        this.EndCoveringFace();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Rest()
     {
+        return;
+    }
+
+    protected override void update()
+    {
+        base.update();
+        switch(this.state)
+        {
+            case StarNosedMoleState.ExposedI:
+                this.ExposedIUpdate();
+                break;
+            case StarNosedMoleState.ExposedII:
+
+                break;
+            case StarNosedMoleState.ExposedIII:
+
+                break;
+            case StarNosedMoleState.Hiding:
+
+                break;
+            case StarNosedMoleState.Tunneling:
+
+                break;
+        }
+    }
+
+    private void ExposedIUpdate()
+    {
+        List<string> funcs = new List<string>()
+        {
+            "ThrowRock",
+            "CoverFace"
+        };
+        List<float> odds = new List<float>()
+        {
+            5f,
+            1f
+        };
         
+        if (!this.busy)
+        {
+            if (this.lastAction != "CoverFace" && this.IsToDoEmpty())
+            {
+                this.AddRandomToDo(funcs, odds);
+            }
+            else
+            {
+                this.AddToDo("ThrowRock");
+            }
+        }
+    }
+
+    private IEnumerator ThrowRock()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            BulletManager.instance.spawn("BasicBullet", new DefaultFireProperties(this.transform.position, this.angleToPlayer(), 4));
+            yield return new WaitForSeconds(.25f);
+        }
+        this.EndAction(0.5f);
+    }
+
+    private IEnumerator CoverFace()
+    {
+        this.BeginCoverFace();
+        yield return new WaitForSeconds(this.coverFaceLength);
+        this.EndCoveringFace();
+        this.EndAction(0.1f);
+    }
+
+    public override void hurt(int damage)
+    {
+        if (!this.coveringFace)
+        {
+            Debug.Log("OUCH");
+            base.hurt(damage);
+        }
+    }
+
+    private void BeginCoverFace()
+    {
+        this.coveringFace = true;
+    }
+
+    private void EndCoveringFace()
+    {
+        this.coveringFace = false;
+    }
+
+    private enum StarNosedMoleState
+    {
+        ExposedI, ExposedII, ExposedIII, Hiding, Tunneling
     }
 }
